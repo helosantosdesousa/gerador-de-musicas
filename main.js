04ee3e312d4d56480482b390c692d9b451d18751
helosantosdesousa <115664843+helosantosdesousa@users.noreply.github.com>
2023-02-01 02:02:29 -0300

    01

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
-// entre 1 e 100
-var numeroSecreto = parseInt(Math.random()*101);
+chute = prompt('Olá usuário :)! Seja bem vindo ao Gerador de músicas da Helô! Escolha um número de 1 à 15 e eu vou te indicar uma música da minha playlist! 🤍 // NOTA: Não se esqueça de atualizar a página à cada música encontrada!');
 
-while (chute != numeroSecreto) {
-    var chute = prompt('Olá jogador, seja bem vindo ao Mentalista! Um jogo em que você deve descobrir o número escolhido pela IA de 0 à 100!. Escolha um número de 0 à 100 :)');
-    
-    if (chute == numeroSecreto){
-        alert ('Acertou! 🎉');
-    }
-    else if (chute > numeroSecreto) 
-    {
-        alert ('você errou! 💀 O número é MENOR que sua escolha ⬇️');
-    }
-    else if (chute < numeroSecreto){
-        alert ('Você errou!💀 O número é MAIOR que sua escolha ⬆️');
-    }
-    
+
+// template alert ("Ótima escolha! Você deveria ouvir: " + "" + " - " + "" + " 🎧")
+
+
+// musica 1 seesaw
+var musUm = 1
+if (chute == musUm){
+    alert ("Ótima escolha! Você deveria ouvir: " + "Trivia: Seesaw" + " - " + "BTS" + " 🎧")
+}
+
+// musica 2 santo
+var musDois = 2
+if (chute == musDois){
+    alert ("Ótima escolha! Você deveria ouvir: " + "Santo" + "-" + "Jão" + " 🎧")
+}
+ // musica 3
+var musTres = 3
+if (chute== musTres){
+    alert ("Ótima escolha! Você deveria ouvir: " + "Levitating" + " - " + "Dua Lipa"
+    + " 🎧")
+ 
+}
+
+var musQuatro = 4
+if (chute== musQuatro){
+    alert ("Ótima escolha! Você deveria ouvir: " + "I hear a symphony" + " - " + "Cody Fry"+ " 🎧")
+}
+
+var musCinco = 5
+if (chute == musCinco){
+    alert ("PARABÉNS🥳! Você desbloqueou a minha música mais ouvida em 2022 :), essa música é muito importante para mim! 💛 Espero que você goste dela <3. Você deveria ouvir: " + "Footloose" + " - " + "Kenny Rogers"+ " 🎧")
+}
+if (chute != musUm && musDois && musTres && musQuatro && musCinco){
+    alert ('Ainda não há músicas para este resultado :(')
+}
+
+// piadinha do capeta
+var musCapeta = 666
+if (chute== musCapeta){
+    alert ("EITA!👹 Você desbloqueou a música da besta e vendeu sua alma... Mas toma aqui uma música pra amenizar a situação. (Ou não hahahah...) Você deveria ouvir: " + "Murder In My Mind" + " - " + "Kordhell" + " 🎧")
 }
